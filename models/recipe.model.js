338d045a27b3f56f0b794374014712d0f2a4cf37
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-17 03:59:47 +0000

    added recipe schema

---
+++ models/recipe.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const recipeSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      required: true,
+    },
+    srevings: {
+      type: Number,
+      required: true,
+    },
+    preppingTime: {
+      type: String,
+      required: true,
+    },
+    cookingTime: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    directions: {
+      type: [String],
+      required: true,
+    },
+    notes: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe
